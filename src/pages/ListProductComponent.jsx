a0c17eff261a022ddee8dc9e4b52bc08ebc3e9da
NhatHuy <ntnh2762002@gmail.com>
2023-10-15 18:30:13 +0700

    nh

--- src/pages/ListProductComponent.jsx
+++ src/pages/ListProductComponent.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import ProductServices from '../services/ProductServices';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faPenToSquare,
+    faEye,
+    faTrash,
+    faPlus
+} from "@fortawesome/free-solid-svg-icons";
 class ListProductComponent extends Component {
     constructor(props) {
         super(props)
@@ -35,109 +41,132 @@ class ListProductComponent extends Component {
 
     render() {
         return (
-            <div className='container'>
-                <h2 className="text-center">Product List</h2>
-                <div className="row">
-                    <button className="btn btn-primary col-md-2" onClick={this.addProduct}> Add Product</button>
-                </div>
-                <br></br>
-                <div className="row">
-                    <table className="table table-striped table-bordered">
-
-                        <thead>
-                            <tr style={{ textAlign: 'center' }}>
-                                <th> PRODUCT IMAGE</th>
-                                <th> PRODUCT NAME</th>
-                                <th> PRODUCT BRAND</th>
-                                <th> PRODUCT PRICE</th>
-                                <th> ACTIONS</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+            <div class="container-xl">
+                <div class="table-responsive">
+                    <div class="table-wrapper">
+                        <div class="table-title">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <h2>Manage <b>Products</b></h2>
+                                </div>
+                                <div class="col-sm-6">
+                                    <a onClick={this.addProduct} class="btn btn-success" data-toggle="modal"><i
+                                        class="material-icons"><FontAwesomeIcon icon={faPlus} /></i> <span style={{ marginTop: '0' }}>Add New Product</span></a>
+                                </div>
+                            </div>
+                        </div>
+                        <table class="table table-striped table-hover">
+                            <span></span>
+                            <thead>
+                                <tr>
+                                    <th style={{ textAlign: 'center' }}>Image</th>
+                                    <th>Name</th>
+                                    <th>Brand</th>
+                                    <th>Price</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.products.map(
+                                        product =>
+                                            <tr key="{product.productId}">
+                                                <td><img style={{ width: '100%' }} src={`/assets/images/${product.imageUrls[0]}`} alt=""
+                                                    srcSet="" /></td>
+                                                <td>{product.name}</td>
+                                                <td> {product.brand}</td>
+                                                <td>{product.price}</td>
+                                                <td>
+                                                    <a onClick={() => this.editProduct(product.productId)} className=""><span><FontAwesomeIcon icon={faPenToSquare} /></span></a>
+                                                    <a style={{ marginLeft: "10px" }} onClick={() =>
+                                                        this.deleteProduct(product.productId)} className=""><span><FontAwesomeIcon icon={faTrash} /></span></a>
+                                                    <a type='button' data-toggle="modal" data-target={`#myModal${product.productId}`}
+                                                        style={{ marginLeft: "10px" }} className=""><span><FontAwesomeIcon icon={faEye} /></span></a>
+                                                </td>
+                                            </tr>
+                                    )
+                                }
+                            </tbody>
                             {
                                 this.state.products.map(
                                     product =>
-                                        <tr key={product.productId}>
-                                            <td style={{ width: '15%' }}><img style={{ width: '100%' }} src={`/assets/images/${product.imageUrls[0]}`} alt="" srcSet="" /></td>
-                                            <td> {product.name} </td>
-                                            <td> {product.brand}</td>
-                                            <td> {product.price}</td>
-                                            <td style={{ textAlign: 'center' }}>
-                                                <button onClick={() => this.editProduct(product.productId)} className="btn btn-info">Update</button>
-                                                <button style={{ marginLeft: "10px" }} onClick={() => this.deleteProduct(product.productId)} className="btn btn-danger">Delete </button>
-                                                <button type='button' data-toggle="modal" data-target={`#myModal${product.productId}`} style={{ marginLeft: "10px" }} className="btn btn-info">View </button>
-                                            </td>
-                                        </tr>
+                                        <div class="container">
+                                            <div key={product.productId} class="modal fade modal-lg " style={{
+                                                maxWidth: '1000px',
+                                                width: '100p0x',
+                                                margin: '0 auto',
+                                                marginTop: '5%',
+                                                paddingRight: '0'
+                                            }} id={`myModal${product.productId}`} role="dialog">
+                                                <div style={{ maxWidth: '700px' }} class="modal-dialog">
 
+                                                    {/* <!-- Modal content--> */}
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                            <h4 class="modal-title"></h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <table>
+                                                                <tr>
+                                                                    <th>Name</th>
+                                                                    <th>Brand</th>
+                                                                    <th>Price</th>
+                                                                    <th>Component</th>
+                                                                    <th>Guide</th>
+                                                                    <th>Instruction</th>
+                                                                    <th>Made in</th>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>{product.name}</td>
+                                                                    <td>{product.brand}</td>
+                                                                    <td>{product.price}</td>
+                                                                    <td>{product.component}</td>
+                                                                    <td>{product.guide}</td>
+                                                                    <td>{product.instruction}</td>
+                                                                    <td>{product.madeIn}</td>
+                                                                </tr>
+                                                            </table>
+                                                            <p><strong>Object: </strong>{product.object}</p>
+                                                            <p><strong>Preservation: </strong>{product.preservation}</p>
+                                                            <p><strong>Store: </strong>{product.store}</p>
+                                                            <p><strong>Virtue: </strong>{product.virtue}</p>
+
+
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
 
                                 )
                             }
+                        </table>
+                        <div class="clearfix">
+                            <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
+                            <ul class="pagination">
+                                <li class="page-item disabled"><a href="#">Previous</a></li>
+                                <li class="page-item"><a href="#" class="page-link">1</a></li>
+                                <li class="page-item"><a href="#" class="page-link">2</a></li>
+                                <li class="page-item active"><a href="#" class="page-link">3</a></li>
+                                <li class="page-item"><a href="#" class="page-link">4</a></li>
+                                <li class="page-item"><a href="#" class="page-link">5</a></li>
+                                <li class="page-item"><a href="#" class="page-link">Next</a></li>
+                            </ul>
+                        </div>
 
-                        </tbody>
-                        {
-                            this.state.products.map(
-                                product =>
-                                    <div class="container">
-                                        <div key={product.productId} class="modal fade modal-lg" style={{
-                                            maxWidth: '800px',
-                                            width: '80%',
-                                            margin: '0 auto',
-                                            marginTop: '5%'
-                                        }} id={`myModal${product.productId}`} role="dialog">
-                                            <div class="modal-dialog">
+                    </div>
 
-                                                {/* <!-- Modal content--> */}
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                        <h4 class="modal-title"></h4>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <table>
-                                                            <tr>
-                                                                <th>Name</th>
-                                                                <th>Brand</th>
-                                                                <th>Price</th>
-                                                                <th>Component</th>
-                                                                <th>Guide</th>
-                                                                <th>Instruction</th>
-                                                                <th>Made in</th>
-                                                            </tr>
-                                                            <tr>
-                                                                <td>{product.name}</td>
-                                                                <td>{product.brand}</td>
-                                                                <td>{product.price}</td>
-                                                                <td>{product.component}</td>
-                                                                <td>{product.guide}</td>
-                                                                <td>{product.instruction}</td>
-                                                                <td>{product.madeIn}</td>
-                                                            </tr>
-                                                        </table>
-                                                        <p><strong>Object: </strong>{product.object}</p>
-                                                        <p><strong>Preservation: </strong>{product.preservation}</p>
-                                                        <p><strong>Store: </strong>{product.store}</p>
-                                                        <p><strong>Virtue: </strong>{product.virtue}</p>
-
-
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                                                    </div>
-                                                </div>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                            )
-                        }
-
-                    </table>
-
-
-                </div >
+                </div>
 
             </div >
+
         )
+
     }
 }
 
